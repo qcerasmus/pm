3c7b3bd7bde65999aa954b810b56f41df5687f9d
Quinton Erasmus <qerasmus@tsti.co.za>
2023-08-14 14:40:55 +0200

    Main ui is done basically

--- pm/main.cpp
+++ pm/main.cpp
@@ -1,44 +1,74 @@
-#include <cstdlib>
-#include <ftxui/dom/node.hpp>
-#include <iostream>
+#include <memory> // for shared_ptr, __shared_ptr_access
+#include <string> // for operator+, to_string
 
-#include "ftxui/dom/elements.hpp"
-#include "ftxui/screen/screen.hpp"
-#include "ftxui/screen/string.hpp"
+#include "clip.h"
+#include "ftxui/component/captured_mouse.hpp" // for ftxui
+#include "ftxui/component/component.hpp"      // for Button, Horizontal, Renderer
+#include "ftxui/component/component_base.hpp" // for ComponentBase
+#include "ftxui/component/event.hpp"
+#include "ftxui/component/screen_interactive.hpp" // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"                 // for separator, gauge, text, Element, operator|, vbox, border
+
+using namespace ftxui;
+
+struct bla
+{
+    std::string label;
+    std::string username;
+    std::string password;
+};
 
 int main()
 {
-    auto summary = [&]
+    std::vector<bla> passwords;
+    passwords.push_back({"reddit", "asdfasdfasdf", "abskdlfjkdsjdklf"});
+    passwords.push_back({"facebook", "asdfasdfasdf", "abskdlfjkdsjdklf"});
+    passwords.push_back({"youtube", "asdfasdfasdf", "abskdlfjkdsjdklf"});
+
+    std::vector<Component> menu_items;
+
+    for (const auto &password : passwords)
     {
-        auto content = ftxui::vbox({
-            ftxui::hbox(
-                {ftxui::text(L"- done:   "), ftxui::text(L"3") | ftxui::bold}) |
-                color(ftxui::Color::Green),
-            ftxui::hbox(
-                {ftxui::text(L"- active: "), ftxui::text(L"2") | ftxui::bold}) |
-                color(ftxui::Color::RedLight),
-            ftxui::hbox(
-                {ftxui::text(L"- queue:  "), ftxui::text(L"9") | ftxui::bold}) |
-                color(ftxui::Color::Red),
+        menu_items.push_back(MenuEntry(password.label));
+    }
+
+    auto status_box = text("press 'u' to copy the username and 'p' to copy the password");
+
+    int selected = 0;
+    auto menu = Container::Vertical(menu_items, &selected);
+    menu |= CatchEvent(
+        [&](Event event)
+        {
+            if (event.character() == "u")
+            {
+                status_box = text("copied username for " + passwords[selected].label + " to clipboard");
+                clip::set_text(passwords[selected].username);
+                return true;
+            }
+            else if (event.character() == "p")
+            {
+                status_box = text("copied password for " + passwords[selected].label + " to clipboard");
+                clip::set_text(passwords[selected].password);
+                return true;
+            }
+
+            return false;
         });
-        return window(ftxui::text(L" Summary "), content);
-    };
 
-    auto document =
-        ftxui::vbox({
-            ftxui::hbox({
-                summary(),
-                summary(),
-                summary() | ftxui::flex,
-            }),
-        });
+    // Modify the way to render them on screen:
+    auto component = Renderer(
+        menu,
+        [&]
+        { return vbox({
+                     text("selected = " + passwords[selected].label) | center,
+                     separator(),
+                     menu->Render() | flex,
+                     separator(),
+                     status_box | center,
+                 }) |
+                 border; });
 
-    document = document | size(ftxui::WIDTH, ftxui::LESS_THAN, 80);
-
-    auto screen = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(document));
-    ftxui::Render(screen, document);
-
-    std::cout << screen.ToString() << '\0' << std::endl;
-
-    return EXIT_SUCCESS;
+    auto screen = ScreenInteractive::TerminalOutput();
+    screen.Loop(component);
+    return 0;
 }
