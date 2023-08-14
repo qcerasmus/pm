5cb019174e6820135c484d54d81b3fd687668dd3
Quinton Erasmus <qerasmus@tsti.co.za>
2023-08-14 15:46:41 +0200

    shit is very broken

--- pm/main.cpp
+++ pm/main.cpp
@@ -18,9 +18,37 @@ struct bla
     std::string password;
 };
 
-int main()
+std::vector<bla> passwords;
+
+Component addNewLoginComponent(std::function<void()> do_nothing, std::function<void()> hide_modal)
 {
-    std::vector<bla> passwords;
+    std::string new_label;
+    std::string new_username;
+    std::string new_password;
+
+    InputOption password_option;
+    password_option.password = true;
+    auto component = Container::Vertical(
+        {Input(&new_label, "label: "),
+         Input(&new_username, "username: "),
+         Input(&new_password, "password: ", password_option),
+         Button("create", [&]()
+                { passwords.push_back({new_label, new_username, new_password}); hide_modal(); }),
+         Button("cancel", [&]()
+                { hide_modal(); })});
+    component |= Renderer(
+        [&](Element inner)
+        {
+            return vbox({text("new login"),
+                         separator(),
+                         inner});
+        });
+
+    return component;
+};
+
+Component mainComponent(std::function<void()> show_modal, std::function<void()> exit)
+{
     passwords.push_back({"reddit", "asdfasdfasdf", "abskdlfjkdsjdklf"});
     passwords.push_back({"facebook", "asdfasdfasdf", "abskdlfjkdsjdklf"});
     passwords.push_back({"youtube", "asdfasdfasdf", "abskdlfjkdsjdklf"});
@@ -51,23 +79,55 @@ int main()
                 clip::set_text(passwords[selected].password);
                 return true;
             }
+            else if (event.character() == "n")
+            {
+                show_modal();
+            }
 
             return false;
         });
 
-    auto component = Renderer(
-        menu,
-        [&]
+    menu |= Renderer(
+        [&](Element inner)
         { return vbox({
                      text("selected = " + passwords[selected].label) | center,
                      separator(),
-                     menu->Render() | flex,
+                     inner | flex,
                      separator(),
                      status_box | center,
                  }) |
                  border; });
+    // auto component = Renderer(
+    //     menu,
+    //     [&]
+    //     { return vbox({
+    //                  text("selected = " + passwords[selected].label) | center,
+    //                  separator(),
+    //                  menu->Render() | flex,
+    //                  separator(),
+    //                  status_box | center,
+    //              }) |
+    //              border; });
+    return menu;
+}
 
+int main()
+{
     auto screen = ScreenInteractive::TerminalOutput();
-    screen.Loop(component);
+
+    auto modal_shown = false;
+
+    auto show_modal = [&]
+    { modal_shown = true; };
+    auto hide_modal = [&]
+    { modal_shown = false; };
+
+    auto exit = screen.ExitLoopClosure();
+    auto do_nothing = [&] {};
+    auto main_component = mainComponent(show_modal, exit);
+    auto modal_component = addNewLoginComponent(do_nothing, hide_modal);
+
+    main_component |= Modal(modal_component, &modal_shown);
+    screen.Loop(main_component);
     return 0;
 }
